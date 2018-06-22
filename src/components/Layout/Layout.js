cad7e1f2dec30d202b70d91bc0b80161601e9287
elshack09 <elshack09@gmail.com>
2018-06-22 14:45:29 -0400

    added burger component & ingredients component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Aux from '../../hoc/Aux'
-
+import classes from './Layout.css'
 
 const layout = (props) => (
     <Aux>
         <div>Toolbar, SideDrawer, Backdrop</div>
-        <main>
+        <main className ={classes.Content}>
             {/* allow us to use the layout component as a wrapper around the core content componets that needs to be rendered to the screen */}
             {props.children}
-        </main>
+        </main> 
     </Aux>
 )
 export default layout
